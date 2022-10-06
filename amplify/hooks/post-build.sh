f292a93a4617398549a8fdd9cf0446f9995ffd14
Nicole Dong <xndong1020@gmail.com>
2022-10-07 06:37:39 +1100

    add amplify hooks

---
+++ amplify/hooks/post-build.sh
@@ -0,0 +1,3 @@
+echo "swtching back to master branch"
+aws amplify update-domain-association --app-id d265i3dhp7tldg --domain-name jeremygu.site --sub-domain-settings "[{\"prefix\": \"www\",\"branchName\": \"master\"}, {\"prefix\": \"\",\"branchName\": \"master\"}]"
+echo "On master branch now"
