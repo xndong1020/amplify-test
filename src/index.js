31692031bee256685b93e381b093d3af3faf053c
Nicole Dong <xndong1020@gmail.com>
2022-10-07 08:10:14 +1100

    minor

--- src/index.js
+++ src/index.js
@@ -10,11 +10,7 @@ import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
