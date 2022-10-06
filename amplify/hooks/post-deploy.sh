b41edfd1b4cf564fce9f6916f2396dfbe9755781
Nicole Dong <xndong1020@gmail.com>
2022-10-07 06:48:36 +1100

    add more hooks

--- amplify/hooks/post-deploy.sh
+++ amplify/hooks/post-deploy.sh
@@ -1,3 +1,3 @@
-echo "Build finished. Swtching to master branch"
+echo "Deployment finished. swtching to master branch"
 aws amplify update-domain-association --app-id d265i3dhp7tldg --domain-name jeremygu.site --sub-domain-settings "[{\"prefix\": \"www\",\"branchName\": \"master\"}, {\"prefix\": \"\",\"branchName\": \"master\"}]"
 echo "On master branch now"
--- amplify/hooks/pre-deploy.sh
+++ amplify/hooks/pre-deploy.sh
@@ -1,3 +1,3 @@
-echo "Build starting. swtching to maintainance branch"
+echo "deployment starting. swtching to maintainance branch"
 aws amplify update-domain-association --app-id d265i3dhp7tldg --domain-name jeremygu.site --sub-domain-settings "[{\"prefix\": \"www\",\"branchName\": \"maintainance\"}, {\"prefix\": \"\",\"branchName\": \"maintainance\"}]" 
 echo "On maintainance branch now"
